8e0426e4aaabcd3b8c0af4fa7ea4d0f93c621934
laravinicius <viniciusfelipe501@hotmail.com>
2026-02-19 18:03:13 +0000

    implantacao

--- bootstrap.php
+++ bootstrap.php
@@ -5,14 +5,9 @@ if (!defined('APP_ROOT')) {
     define('APP_ROOT', __DIR__);
 }
 
-/**
- * Sessão: configurações só podem ser feitas ANTES da sessão iniciar.
- * Então: aplica ini_set apenas se ainda não iniciou.
- */
 if (session_status() === PHP_SESSION_NONE) {
     ini_set('session.gc_maxlifetime', '28800');
     ini_set('session.cookie_lifetime', '28800');
-
     session_start();
 }
 
