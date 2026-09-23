f059920b99debbb7c0ecffbe25daf8cee452f677
Vinicius Lara <103464054+laravinicius@users.noreply.github.com>
2026-09-23 14:52:40 -0300

    update database settings

--- app/config/database.php
+++ app/config/database.php
@@ -11,13 +11,14 @@ if (file_exists($envPath)) {
 }
 
 $host = getenv('DB_HOST') ?: 'localhost';
+$port = getenv('DB_PORT') ?: '3306';
 $db   = getenv('DB_NAME') ?: 'microgate_db';
 $user = getenv('DB_USER') ?: 'root';
 $pass = getenv('DB_PASS') ?: '';
 
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
